ed25afb0c6988652fc01dc20c407e3219fea7373
DeividCezar <perspicilum@gmail.com>
2024-09-27 20:16:31 -0300

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "lanchonete-lambdas-bucket"
-    key            = "lanchonete-lambda-pre-sign-up/terraform.tfstate"
-    region         = "us-east-1"
+    bucket = "lanchonete-lambdas-bucket"
+    key    = "lanchonete-lambda-pre-sign-up/terraform.tfstate"
+    region = "us-east-1"
   }
 }
